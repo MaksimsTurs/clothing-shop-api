d33ad29776b20fb90d3b7367289211a95eb679b6
Maksims Turs <maksims07turs@gmail.com>
2023-12-22 01:13:30 +0100

    Initial commit

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,15 +1,19 @@
 import cloudinary from 'cloudinary'
+import cloudinaryConf from '../config/cloudinarySetup.js'
 
 const user = {
   registration: async (req, res) => {
     console.log('[SERVER]: Registration')
 
     try {
+      cloudinaryConf
       const d = await cloudinary.v2.uploader.upload(req.files[0].path)
       return res.status(200).send({ url: d.secure_url })
     } catch(e) {
       console.log(e)
     }
+
+    return res.status(200).send({ message: 'nope' })
   }
 }
 
