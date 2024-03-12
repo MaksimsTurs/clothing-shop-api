3992c347d5b86195e61046ec4edd953420222601
Maksims Turs <maksims07turs@gmail.com>
2024-03-12 19:08:38 +0100

    Bug fixes

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -9,6 +9,7 @@ export default async function connectDB() {
   try {
     await connect(process.env.MONGO_DB_URI)
     loger.logCustomText('Connected to MongoDB!', true)
+    loger.logCustomText(`DB URL: ${process.env.MONGO_DB_URI}`)
   } catch(error) {
     loger.logError(error, import.meta.url, '12 - 13')
   }
