3ede2219f700d7d4ded55d8ffc188bf3b183d030
Maksims Turs <maksims07turs@gmail.com>
2024-05-13 20:13:48 +0200

    Update modals

--- src/model/productModel.js
+++ src/model/productModel.js
@@ -3,8 +3,8 @@ import { model, Schema } from 'mongoose'
 export default model('products', new Schema({
 	_id: 					{ type: Schema.Types.ObjectId },
 	sectionID: 		{ type: String, ref: 'product-sections', default: null },
-	title: 				{ type: String, default: null },
-	description: 	{ type: String, default: null },
+	title: 				{ type: String, default: '' },
+	description: 	{ type: String, default: '' },
 	category: 		{ type: String, default: null },
 	price: 				{ type: Number, default: 0 },
 	stock: 				{ type: Number, default: 0 },
